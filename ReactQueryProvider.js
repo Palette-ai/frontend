86ca33ea8cf4740a4ce939ec06878241d14dad35
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2021-02-07 13:26:28 -0500

    inital setup and navigation complete

---
+++ ReactQueryProvider.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	QueryClient,
+	QueryClientProvider,
+} from 'react-query'
+
+
+const queryClient = new QueryClient()
+
+function ReactQueryProvider({ children }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	)
+}
+
+export default ReactQueryProvider	
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { createBottomTabNavigator } from 'react-navigation-bottom-tabs-no-warnings'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import Discover from '../pages/Discover'
+import Home from '../pages/Home'
+import Profile from '../pages/Profile'
+import Social from '../pages/Social'
+import Map from '../pages/Map'
+
+const Tab = createBottomTabNavigator()
+export default function AppNavigator() {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name={'Home'}
+				component={Home}
+				options={{
+					tabBarIcon: ({ color, size }) =>
+						<MaterialCommunityIcons name="home" color={color} size={size} />
+				}}
+			/>
+			<Tab.Screen
+				name={'Discover'}
+				component={Discover}
+				options={{
+					tabBarIcon: ({ color, size }) =>
+						<MaterialCommunityIcons name="cloud-search-outline" color={color} size={size} />
+				}}
+			/>
+			<Tab.Screen
+				name={'Map'}
+				component={Map}
+				options={{
+					tabBarIcon: ({ color, size }) =>
+						<MaterialCommunityIcons name="account-group" color={color} size={size} />
+				}}
+			/>
+			<Tab.Screen
+				name={'Social'}
+				component={Social}
+				options={{
+					tabBarIcon: ({ color, size }) =>
+						<MaterialCommunityIcons name="account-group" color={color} size={size} />
+				}}
+			/>
+			<Tab.Screen
+				name={'Profile'}
+				component={Profile}
+				options={{
+					tabBarIcon: ({ color, size }) =>
+						<MaterialCommunityIcons name="account" color={color} size={size} />
+				}}
+			/>
+		</Tab.Navigator>
+	)
+}
