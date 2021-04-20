aa067952191728a6be45a48085de47d2e9da289c
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2021-04-20 15:32:17 -0400

    fixed discover loading

--- pages/Discover.js
+++ pages/Discover.js
@@ -5,7 +5,7 @@ import {
 	Text,
 	View,
 } from 'react-native'
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useLazyQuery } from '@apollo/client';
 import { TouchableOpacity } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
 import { GET_ALL_DISHES, GET_SOME_DISHES } from '../queries/dishes';
@@ -19,53 +19,50 @@ import DishCard from '../components/Discover/DishCard';
 import axios from 'axios';
 
 const Discover = ({ navigation }) => {
-
-	const [dishRecs, setDishRecs] = useState('')
+	const [getDishes, { loading, data, error }] = useLazyQuery(GET_SOME_DISHES)
 
 	const userIDString = firebase.auth().currentUser.photoURL;
 	useEffect(() => {
 		axios.post("https://palette-backend.herokuapp.com/rec", {
 			user_id: userIDString
 		})
-		.then(res => {
-			let recDishes = res.data.map(d => mongoose.Types.ObjectId(d))
-			setDishRecs(recDishes)
-		})
-		.catch(e => {
-			console.log(e)
-		})
+			.then(res => {
+				getDishes({
+					variables: {
+						_ids: res.data.map(d => mongoose.Types.ObjectId(d))
+					},
+				})
+			})
+			.catch(e => {
+				console.log(e)
+			})
 	}, [])
-	
 
-	const { loading, error, data } = useQuery(GET_SOME_DISHES, {
-		variables: {
-			_ids: dishRecs
-		},
-		})
 
 	if (loading) return <Text>Loading...</Text>
 	if (error) return <Text>Not good why did it break...</Text>
 
-
-	return (
-		<View syle={styles.container}>
-			{/* TODO: Replace search UI with search and filter functionality */}
-			<Search />
-			<View style={styles.item_container}>
-				<ScrollView showsVerticalScrollIndicator={false}>
-					{data.dishByIds.map(dish => (
-						<TouchableOpacity
-							activeOpacit={0.1}
-							onPress={() => navigation.navigate('Dish', { dish, navigation })}
-							key={dish._id}
-						>
-							<DishCard dish={dish} />
-						</TouchableOpacity>
-					))}
-				</ScrollView>
+	return !data ?
+		<Text>hi</Text> :
+		(
+			<View syle={styles.container}>
+				{/* TODO: Replace search UI with search and filter functionality */}
+				<Search />
+				<View style={styles.item_container}>
+					<ScrollView showsVerticalScrollIndicator={false}>
+						{data.dishByIds.map(dish => (
+							<TouchableOpacity
+								activeOpacit={0.1}
+								onPress={() => navigation.navigate('Dish', { dish, navigation })}
+								key={dish._id}
+							>
+								<DishCard dish={dish} />
+							</TouchableOpacity>
+						))}
+					</ScrollView>
+				</View>
 			</View>
-		</View>
-	)
+		)
 }
 
 const styles = StyleSheet.create({
