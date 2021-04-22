faf524bf0122709725b2a9465ded69e1e2a0883e
Keene Morawitz <keenemorawitz@Keenes-MacBook-Pro.local>
2021-04-22 16:53:30 -0400

    On way to just scrolling through reccomended. Will update with liked after relationship built in query

--- pages/LikedDishes.js
+++ pages/LikedDishes.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Like from '../components/Liked/Like'
 import DishCard from '../components/Discover/DishCard';
 import firebase from 'firebase/app';
 import mongoose from 'mongoose';
+import axios from 'axios';
+import { GET_SOME_DISHES } from '../queries/dishes';
+import { useLazyQuery } from '@apollo/client';
 
-const LikedDishes = () => {
-	return (
+const LikedDishes = ({ navigation }) => {
+	const [getDishes, { loading, data, error }] = useLazyQuery(GET_SOME_DISHES)
+
+	const userIDString = firebase.auth().currentUser.photoURL;
+	useEffect(() => {
+		axios.post("https://palette-backend.herokuapp.com/rec", {
+			user_id: userIDString
+		})
+			.then(res => {
+				getDishes({
+					variables: {
+						_ids: res.data.map(d => mongoose.Types.ObjectId(d))
+					},
+				})
+			})
+			.catch(e => {
+				console.log(e)
+			})
+	}, [])
+
+	if (loading) return <Text>Loading...</Text>
+	if (error) return <Text>Not good why did it break...</Text>
+
+	return !data ?
+	(
 		<View syle={styles.container}>
 			<View>
 				<Like/>
+				<View style={styles.item_container}>
+					<ScrollView showsVerticalScrollIndicator={false}>
+						{data.dishByIds.map(dish => (
+							<TouchableOpacity
+								activeOpacit={0.1}
+								onPress={() => navigation.navigate('Dish', { dish, navigation })}
+								key={dish._id}
+							>
+								<DishCard dish={dish} />
+							</TouchableOpacity>
+						))}
+					</ScrollView>
+				</View>
 			</View>
 		</View>
 	)
