ca254b26b69484642119b31d6b5a246a9f9d7768
Keene Morawitz <keenemorawitz@Keenes-MacBook-Pro.local>
2021-04-22 16:09:21 -0400

    Added Liked Header to Liked Dish Page

--- pages/LikedDishes.js
+++ pages/LikedDishes.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Like from '../components/Liked/Like'
+import DishCard from '../components/Discover/DishCard';
+import firebase from 'firebase/app';
+import mongoose from 'mongoose';
 
 const LikedDishes = () => {
 	return (
-		<SafeAreaView style={styles.container}>
+		<View syle={styles.container}>
 			<View>
-				<Text>DIS BE DA Social</Text>
+				<Like/>
 			</View>
-		</SafeAreaView>
+		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#FDFCFC',
 	},
 });
 
